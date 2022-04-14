4754e66d5a28dbcfd5674227235677ce3860820d
Valdirley <68399095+Valdirley@users.noreply.github.com>
2022-04-14 19:15:43 -0300

    Add files via upload

--- src/Fila_Circ.js
+++ src/Fila_Circ.js
@@ -1,71 +1,89 @@
 class Fila_Circ {
 
-    constructor(MaxSize=10) {
-      this.capacidade = MaxSize
-      this.dados = []
-      this.inicio = 0
-      this.fim = -1
+    constructor(t = 10) {
+      this.capacidade = t;
+      this.dados = [];
+      this.inicio = 1;
+      this.fim = 1;
     }
 
     enqueue(dado) {
       if(this.isFull()){
-        throw new Error ("OverFlow")
+        throw new Error ("OverFlow");
       } else {
-        this.dados[this.fim] = dado
+        this.dados[this.fim] = dado;
         if (this.fim  === this.capacidade){
           this.fim = 0;
         } else {
-          this.fim = this.fim + 1
+          this.fim = this.fim + 1;
         }
       }
     }
 
     dequeue(){
         if (this.isEmpty()){
-          throw new error ("UnderFlow")
+          throw new Error ("UnderFlow");
         } else {
-          let result = this.dados [this.inicio]
+          let result = this.dados [this.inicio];
           if (this.inicio === this.capacidade) {
-            this.inicio = 0
+            this.inicio = 0;
           } else {
-            this.inicio = this.inicio + 1
+            this.inicio = this.inicio + 1;
           }
-          return result
+          return result;
         }
     }
 
     front(){
         if(!this.isEmpty()) {
-          return this.dados[this.inicio]
+          return this.dados[this.inicio];
         }
     }
 
     clear(){
-        this.inicio = 1
-        this.fim = 1
+        this.inicio = 1;
+        this.fim = 1;
     }
 
     toString(){
+      let resultado = "[";
+
+        if (this.fim>this.inicio) {
+          for (let i=this.inicio; i<this.fim; i++)
+            resultado += `${this.dados[i]}`
+            } else { 
+              for (let i=this.inicio; i<=this.capacidade; i++)
+            resultado += `${this.dados[i]}`
+          }
+            if (this.fim<this.inicio) {
+              for (let i=0; i<this.fim; i++)
+              resultado += `${this.dados[i]}`
+            }
+        resultado += "]";
+
+        return resultado;
 
     }
 
     length() {
         if(this.isEmpty()){
-          return 0
+          return 0;
         } else if (this.inicio < this.fim){
-          return this.fim - this.inicio
+          return this.fim - this.inicio;
         } else {
-          return this.capacidade - this.inicio + this.fim + 1
+          return this.capacidade - this.inicio + this.fim + 1;
         }
     }
       
 
     isEmpty() {
-        return this.inicio === this.fim
+        return this.inicio === this.fim;
     }
 
     isFull(){
-        return this.inicio === this.fim + 1
+      if((this.inicio===0)&(this.fim===this.capacidade)){
+        throw new Error ("OverFlow");
+      } else  return this.inicio === this.fim + 1;
     }
 }
 
